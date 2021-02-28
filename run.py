0d0a362255e3ce989fc6c8dd4c554dc4c43fbb59
zx85 <git@mus-ic.co.uk>
2021-02-28 18:56:55 +0000

    changing port for sense

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 from cam_indicator import app 
 
 if __name__ == '__main__':
-    app.run(debug=True,host='0.0.0.0',port=6000)
+    app.run(debug=True,host='0.0.0.0',port=5050)
 #    app.run(host='0.0.0.0')
